a5cd4ab7a492e55f70e862a0be8d7ea6afab4c04
wangleiasia <517424651@qq.com>
2018-05-22 22:29:15 +0800

    增加违规描述

---
+++ sonar/src/main/java/com/yubing/web/interceptor/SqlCostInterceptor.java
@@ -0,0 +1,43 @@
+package com.yubing.web.interceptor;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.log4j.Logger;
+
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * Sql执行                                 时间记录拦截器
+ */
+@Intercepts({@Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
+@Signature(type = StatementHandler.class, method = "update", args = {Statement.class}),
+@Signature(type = StatementHandler.class, method = "batch", args = { Statement.class })})
+public class SqlCostInterceptor implements Interceptor {
+
+    private static final Logger LOGGER = Logger.getLogger(SqlCostInterceptor.class);
+
+    public Object intercept(Invocation invocation) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        try {
+            return invocation.proceed();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            long sqlCost = endTime - startTime;
+            LOGGER.debug("执行耗时 : [" + sqlCost + "ms ] ");
+        }
+    }
+
+    public Object plugin(Object o) {
+        return Plugin.wrap(o, this);
+    }
+
+    public void setProperties(Properties properties) {
+
+    }
+}
